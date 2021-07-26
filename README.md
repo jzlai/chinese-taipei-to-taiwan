afe4c633e15f396317dc6c80082ef80463e1fb64
jzlai <jun-zhe.lai@spring-media.de>
2021-07-26 13:05:28 +0200

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Chinese Taipei to Taiwan
+
+Chrome extension that replaces "Chinese Taipei" with "Taiwan 🇹🇼"
+
+## Installation
+
+1. Download project and unzip
+2. Go to Chrome Extensions Settings
+3. Click `Load unpacked` and select root folder
---
+++ script.js
@@ -0,0 +1,19 @@
+const replaceFn = () => {
+  const text = document.querySelectorAll(
+    "h1, h2, h3, h4, h5, p, li, td, caption, span, a, p, div"
+  );
+  const regexp = /Chinese Taipei/gi;
+  for (const element of text) {
+    if (element.innerHTML.match(regexp)) {
+      element.innerHTML = element.innerHTML.replace(regexp, "Taiwan 🇹🇼");
+    }
+  }
+};
+
+let timeout = null;
+document.addEventListener("DOMSubtreeModified", () => {
+  if (timeout) {
+    clearTimeout(timeout);
+  }
+  timeout = setTimeout(replaceFn, 500);
+});
