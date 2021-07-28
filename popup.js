602d2ee4864a648741edc09303f21504ffba9eb9
Jun-Zhe Lai <hi@junzhe.me>
2021-07-28 23:14:17 +0200

    reload page on disable

--- popup.js
+++ popup.js
@@ -1,10 +1,23 @@
-function switchEnabled() {
+async function onClick() {
   const checkbox = document.getElementById('checkbox');
+
   chrome.storage.sync.set({ enabled: checkbox.checked });
+
+  if (!checkbox.checked) {
+    const currentTab = await getCurrentTab();
+    await chrome.tabs.reload(currentTab.id);
+    console.log(currentTab);
+  }
+}
+
+async function getCurrentTab() {
+  const queryOptions = { active: true, currentWindow: true };
+  const [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
 }
 
 chrome.storage.sync.get('enabled', ({ enabled }) => {
   document.getElementById('checkbox').checked = enabled;
 });
 
-document.getElementById('checkbox').addEventListener('click', switchEnabled);
+document.getElementById('checkbox').addEventListener('click', onClick);
